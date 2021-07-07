9e4a1dea1b640138da7e6362cdf7b7bb61373d8b
Gerelt-Od <gereltod.b@gmail.com>
2021-07-07 17:46:02 +0800

    Deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import profileimage from "./img/gia-pic.jpg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <h2>Author: GereltOd Baatar</h2>
+      <img src={profileimage} alt="gia-logo" />
     </div>
   );
 }
